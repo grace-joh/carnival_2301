e3ee464f112cd14d7b9a5dcc87bb88eb38e54752
Grace Joh <graceehjoh@gmail.com>
2023-02-27 12:56:39 -0600

    feat: Add Carnival #visitor_count, #summary_report, #visitors, #favorite_ride

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -23,4 +23,49 @@ class Carnival
     @rides.each { |ride| total += ride.total_revenue }
     total
   end
+
+  def visitor_count
+    visitor_array = []
+    @rides.each do |ride|
+      ride.rider_log.each_key { |visitor| visitor_array |= visitor }
+    end
+    visitor_array.size
+  end
+
+  def summary_report
+    {
+      'Number of Visitors': visitor_count,
+      'Revenue Earned': total_revenue,
+      'List of Visitors': visitors
+    }
+  end
+
+  def visitors
+    visitor_hash = {}
+    @rides.each do |ride|
+      ride.rider_log.each_key do |visitor|
+        visitor_hash[visitor.name] =
+          {
+            'Favorite Ride': favorite_ride[visitor]
+            # 'Total Spent': 0
+          }
+      end
+    end
+    visitor_hash
+  end
+
+  def favorite_ride
+    favorite_hash = {}
+    @rides.each do |ride|
+      ride.rider_log.each_key { |visitor| favorite_hash[visitor] = nil }
+    end
+    favorite_hash.each_key do |visitor|
+      max_times_ridden = 0
+      @rides.each do |ride|
+        times_ridden = ride.rider_log[visitor]
+        favorite_hash[visitor] = ride if times_ridden > max_times_ridden
+      end
+    end
+    favorite_hash
+  end
 end
